71b87744f9faa2065efdc0c15f0bad32a52c9121
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-06-02 23:51:41 -0700

    s11

---
+++ pdf/app/chat/callbacks/stream.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List
+from uuid import UUID
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain_core.messages import BaseMessage
+
+class StreamingHandler(BaseCallbackHandler):
+    def __init__(self, queue) -> None:
+        self.queue = queue
+        self.streaming_run_ids = set()
+
+    def on_chat_model_start(self, serialized, messages, run_id, **kwargs: Any):
+        if serialized["kwargs"]["streaming"]:
+            self.streaming_run_ids.add(run_id)
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.queue.put(token)
+    
+    def on_llm_end(self, response, run_id, **kwargs):
+        if run_id in self.streaming_run_ids:
+            self.queue.put(None)
+            self.streaming_run_ids.remove(run_id)
+
+    def on_llm_error(self, error, **kwargs):
+        self.queue.put(None)
---
+++ pdf/test.py
@@ -0,0 +1,60 @@
+from typing import Any
+from uuid import UUID
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
+from dotenv import load_dotenv
+from queue import Queue
+from threading import Thread
+
+from langchain_core.outputs import LLMResult
+
+
+load_dotenv()
+
+class StreamingHandler(BaseCallbackHandler):
+    def __init__(self, queue) -> None:
+        self.queue = queue
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.queue.put(token)
+    
+    def on_llm_end(self, response, **kwargs):
+        self.queue.put(None)
+
+    def on_llm_error(self, error, **kwargs):
+        self.queue.put(None)
+
+chat = ChatOpenAI(
+    streaming=True
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("human", "{content}")
+])
+
+class StreamableChain:
+    def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
+
+        def task():
+            self(input, callbacks=[handler])
+        
+        Thread(target=task).start()
+
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+            yield token
+
+class StreamingChain(StreamableChain, LLMChain):
+    pass
+
+chain = StreamingChain(llm=chat, prompt=prompt)
+
+for output in chain.stream(input={"content":"tell me a joke"}):
+    print(output)
+
